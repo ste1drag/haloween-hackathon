19ac8deaafe0001a7ec7ac87cd9e5ad8affa960b
Stevan Dragović <stevandragovic.10@gmail.com>
2021-11-05 18:09:32 +0100

    Dodato dugme za pocetak igre kao i unosenje username-a

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,11 +5,11 @@ function Card(props){
     return(
         <ReactCardFlip isFlipped={props.isFlipped} flipDirection="vertical">
             <div>
-                <img src={props.imgSrc1} alt="frontImage" style={{height:'250px',width:'125px'}} onClick={props.onClick}/>
+                <img src={props.imgSrc1} alt="frontImage" style={{height:'175px',width:'90px'}} onClick={props.onClick}/>
             </div>
     
             <div>
-                <img src={props.imgSrc2} alt="frontImage" style={{height:'250px',width:'125px'}} onClick={props.onClick}/>
+                <img src={props.imgSrc2} alt="frontImage" style={{height:'175px',width:'90px'}} onClick={props.onClick}/>
             </div>
         </ReactCardFlip>
 
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -11,14 +11,22 @@ import bat from "../images/bat.png";
 import pumpkin from "../images/pumpkin.png";
 
 
-let cardsArray=[{ "id": 1, "imgSrc1": backgroundImg, "imgSrc2": bat, "isFlipped": false, "isMatched": false },
-{ "id": 2, "imgSrc1": backgroundImg, "imgSrc2": candy, "isFlipped": false, "isMatched": false },
-{ "id": 3, "imgSrc1": backgroundImg, "imgSrc2": ghost, "isFlipped": false, "isMatched": false },
-{ "id": 4, "imgSrc1": backgroundImg, "imgSrc2": castle, "isFlipped": false, "isMatched": false },
-{ "id": 5, "imgSrc1": backgroundImg, "imgSrc2": hand, "isFlipped": false, "isMatched": false },
-{ "id": 6, "imgSrc1": backgroundImg, "imgSrc2": happy, "isFlipped": false, "isMatched": false },
-{ "id": 7, "imgSrc1": backgroundImg, "imgSrc2": hat, "isFlipped": false, "isMatched": false },
-{ "id": 8, "imgSrc1": backgroundImg, "imgSrc2": pumpkin, "isFlipped": false, "isMatched": false },
+let cardsArray=[{ "id": 1,"key":1,"imgSrc1": backgroundImg, "imgSrc2": bat, "isFlipped": false, "isMatched": false },
+{ "id": 2,"key":2, "imgSrc1": backgroundImg, "imgSrc2": candy, "isFlipped": false, "isMatched": false },
+{ "id": 3,"key":3, "imgSrc1": backgroundImg, "imgSrc2": ghost, "isFlipped": false, "isMatched": false },
+{ "id": 4, "key":4,"imgSrc1": backgroundImg, "imgSrc2": castle, "isFlipped": false, "isMatched": false },
+{ "id": 5, "key":5,"imgSrc1": backgroundImg, "imgSrc2": hand, "isFlipped": false, "isMatched": false },
+{ "id": 6,"key":6, "imgSrc1": backgroundImg, "imgSrc2": happy, "isFlipped": false, "isMatched": false },
+{ "id": 7,"key":7, "imgSrc1": backgroundImg, "imgSrc2": hat, "isFlipped": false, "isMatched": false },
+{ "id": 8,"key":8, "imgSrc1": backgroundImg, "imgSrc2": pumpkin, "isFlipped": false, "isMatched": false },
+{ "id": 9, "key":1,"imgSrc1": backgroundImg, "imgSrc2": bat, "isFlipped": false, "isMatched": false },
+{ "id": 10,"key":2, "imgSrc1": backgroundImg, "imgSrc2": candy, "isFlipped": false, "isMatched": false },
+{ "id": 11,"key":3, "imgSrc1": backgroundImg, "imgSrc2": ghost, "isFlipped": false, "isMatched": false },
+{ "id": 12,"key":4, "imgSrc1": backgroundImg, "imgSrc2": castle, "isFlipped": false, "isMatched": false },
+{ "id": 13,"key":5, "imgSrc1": backgroundImg, "imgSrc2": hand, "isFlipped": false, "isMatched": false },
+{ "id": 14,"key":6, "imgSrc1": backgroundImg, "imgSrc2": happy, "isFlipped": false, "isMatched": false },
+{ "id": 15,"key":7, "imgSrc1": backgroundImg, "imgSrc2": hat, "isFlipped": false, "isMatched": false },
+{ "id": 16,"key":8, "imgSrc1": backgroundImg, "imgSrc2": pumpkin, "isFlipped": false, "isMatched": false },
 ];
 
 
@@ -29,7 +37,10 @@ let cardsArray=[{ "id": 1, "imgSrc1": backgroundImg, "imgSrc2": bat, "isFlipped"
 function Game(){
     const gridContainer={display:'grid',gridGap:'50px',gridTemplateColumns:'auto auto auto auto'};
 
-    const [cards,setCards]=useState([...cardsArray,...cardsArray]);
+    const [cards,setCards]=useState(cardsArray);
+    const [timer,setTimer]=useState(60);
+    const [isStarted,setStarted]=useState(false);
+    const [username,setUsername]=useState("");
 
 
     function handleClick(id){
@@ -39,15 +50,37 @@ function Game(){
         setCards([...cardsChange]);
     }
 
+    function startGame(){
+        setStarted(true);
+        
+    }
+
 
 
     let rows=[];
     cards.map(card=>rows.push(<Card id={card.id} imgSrc1={card.imgSrc1} imgSrc2={card.imgSrc2} 
         isFlipped={card.isFlipped} isMatched={card.isMatched} onClick={()=>handleClick(card.id)}/>));
+
     return(
-        <div style={gridContainer}>
-            {rows}
-            
+        <div>
+            {isStarted ?
+             <div>
+                <div>
+                   {username}
+                </div>
+                <div style={gridContainer}>
+                   {rows}
+                </div>
+            </div>
+             : 
+             <div>
+
+                 <input type="text" onChange={(e)=>setUsername(e.target.value)} />
+                 <button onClick={()=>startGame()}>Zapocni igru</button>
+
+             </div> 
+             }
+        
         </div>
     )
 }
